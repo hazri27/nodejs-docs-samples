4b8b74f31da365679b3c85c408bc43405341f476
hazri27 <57011176+hazri27@users.noreply.github.com>
2020-09-10 15:41:17 +0300

    Update index.js

--- functions/scheduleinstance/index.js
+++ functions/scheduleinstance/index.js
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
- 
+ //comment
 // [START functions_start_instance_pubsub]
 // [START functions_stop_instance_pubsub]
 // Imports the Google Cloud client library
@@ -20,20 +20,26 @@ const {Resource} = require('@google-cloud/resource');
 // Creates a client
 const resource = new Resource();
 const Compute = require('@google-cloud/compute');
-var tst_array = [];
-var count = 0; 
+
+/*
 async function quickstart() {
   // Lists current projects
   const [projects] = await resource.getProjects();
  
-  console.log('Projects:');
+  console.log('Current Projects are:');
+ 
   projects.forEach(project => {
     tst_array[count]=project.id;
+    console.log("ab_projects= "+ tst_array[count]);
     count = count+1;
     });
+
+    console.log('quickstart completed line 37 (we expect2)=  ' + count);
 }
-quickstart();
- 
+*/
+
+//to be moved inside pub/sub quickstart();
+
 /**
  * Starts Compute Engine instances.
  *
@@ -48,25 +54,47 @@ quickstart();
  */
 exports.startInstancePubSub = async (event, context, callback) => {
   try {
-   for (var i = 0; i < tst_array.length; i++){
+    var tst_array = [];
+    var count = 0; 
+    //copied from quickstart
+    const [projects] = await resource.getProjects();
+ 
+  console.log('Current Projects are:');
+ 
+  projects.forEach(project => {
+    tst_array[count]=project.id;
+    console.log("ab_projects= "+ tst_array[count]);
+    count = count+1;
+    });
+
+    console.log('quickstart completed line 70 (we expect2)=  ' + count);
+
+    //////////end of quickstart copy
+
+
+    console.log('during startinstancepubsub line75 (we expect2)=  ' + count);
+   for (var i = 0; i < count; i++){
+     console.log("during for iterations line 57");
     const compute = new Compute({
     projectId: tst_array[i]
  //   projectId: 'ab-gcp-tst-common-kafka-tst'
  
 });
-const zone = compute.zone('europe-west3-c');
+
+
+//const zone = compute.zone('europe-west3-c');
 // [END functions_stop_instance_pubsub]
 
 
  
-zone.getVMs().then(function(data) {
-  const vms = data[0];
-});
+//zone.getVMs().then(function(data) {
+  //const vms = data[0];
+//});
     const payload = _validatePayload(
       JSON.parse(Buffer.from(event.data, 'base64').toString())
     );
     const options = {filter: `labels.${payload.label}`};
-    const [vms] = await zone.getVMs(options);
+    const [vms] = await compute.getVMs(options);
     await Promise.all(
       vms.map(async (instance) => {
         if (payload.zone === instance.zone.id) {
@@ -85,47 +113,66 @@ zone.getVMs().then(function(data) {
     const message = `Successfully started instance(s)`;
     console.log(message);
     callback(null, message);
-  }} catch (err) {
-    console.log(err);
+  }
+  
+  console.log("for iterations should be completed");
+  console.log('line99 (we expect2)=  ' + count);
+  
+  
+  
+  } catch (err) {
+    console.log("catching errors line 104");
+    console.log(err);  
     callback(err);
   }
 };
 // [END functions_start_instance_pubsub]
 // [START functions_stop_instance_pubsub]
  
-/**
- * Stops Compute Engine instances.
- *
- * Expects a PubSub message with JSON-formatted event data containing the
- * following attributes:
- *  zone - the GCP zone the instances are located in.
- *  label - the label of instances to stop.
- *
- * @param {!object} event Cloud Function PubSub message event.
- * @param {!object} callback Cloud Function PubSub callback indicating completion.
- */
+
 exports.stopInstancePubSub = async (event, context, callback) => {
   try {
-    for (var i = 0; i < tst_array.length; i++){
+    var tst_array = [];
+    var count = 0; 
+    //copied from quickstart
+    const [projects] = await resource.getProjects();
+ 
+  console.log('Current Projects are:');
+ 
+  projects.forEach(project => {
+    tst_array[count]=project.id;
+    console.log("ab_projects= "+ tst_array[count]);
+    count = count+1;
+    });
+
+    console.log('quickstart completed line 70 (we expect2)=  ' + count);
+
+    //////////end of quickstart copy
+
+
+    console.log('during stopinstancepubsub line75 (we expect2)=  ' + count);
+   for (var i = 0; i < count; i++){
+     console.log("during for iterations line 57");
     const compute = new Compute({
     projectId: tst_array[i]
  //   projectId: 'ab-gcp-tst-common-kafka-tst'
  
 });
-const zone = compute.zone('europe-west3-c');
+
+
+//const zone = compute.zone('europe-west3-c');
 // [END functions_stop_instance_pubsub]
 
 
  
-zone.getVMs().then(function(data) {
-  const vms = data[0];
-});
+//zone.getVMs().then(function(data) {
+ // const vms = data[0];
+//});
     const payload = _validatePayload(
       JSON.parse(Buffer.from(event.data, 'base64').toString())
     );
-    
     const options = {filter: `labels.${payload.label}`};
-    const [vms] = await zone.getVMs(options);
+    const [vms] = await compute.getVMs(options);
     await Promise.all(
       vms.map(async (instance) => {
         if (payload.zone === instance.zone.id) {
@@ -136,8 +183,6 @@ zone.getVMs().then(function(data) {
  
           // Operation pending
           return operation.promise();
-        } else {
-          return Promise.resolve();
         }
       })
     );
@@ -146,13 +191,21 @@ zone.getVMs().then(function(data) {
     const message = `Successfully stopped instance(s)`;
     console.log(message);
     callback(null, message);
-  }} catch (err) {
-    console.log(err);
+  }
+  
+  console.log("for iterations should be completed");
+  console.log('line99 (we expect2)=  ' + count);
+  
+  
+  
+  } catch (err) {
+    console.log("catching errors line 104");
+    console.log(err);  
     callback(err);
   }
-};
-// [START functions_start_instance_pubsub]
- 
+}; 
+
+
 /**
  * Validates that a request payload contains the expected fields.
  *
@@ -169,3 +222,6 @@ const _validatePayload = (payload) => {
 };
 // [END functions_start_instance_pubsub]
 // [END functions_stop_instance_pubsub]
+
+
+
